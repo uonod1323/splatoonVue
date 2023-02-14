04c91974bda0820cc16b8b621a2186b853cf3f48
uonod1323 <uonod1323@naver.com>
2023-02-14 17:30:59 +0900

    슬로셔 추가

--- src/components/WeapontypePage.vue
+++ src/components/WeapontypePage.vue
@@ -1,9 +1,13 @@
 <template>
-    야임마!
-    {{  }}
-    <div>
-        <p v-html="question"></p>
+   <div class = "container">
+    <div class="content-container">
+      <div class="question-container">
+        <img :src = this.weaponImg />
+        <p>{{ this.weaponName }}</p>
+        <p class="result-content" v-html="weaponContent"></p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -11,19 +15,34 @@ export default {
     name: 'WeapontypePage',
     data(){
       return {
+        weaponName    : '',
+        weaponContent : '',
+        weaponImg     : '',
+
         weaponArray : [
             {
-                name : '슬로셔',
-                content : '당신에게는 정면 싸움보다 좋은 위치를 선점하고 일방적으로 목표를 타격하는 슬로셔가 어울립니다!',
-                img : '',
+                name : 'slosher',
+                name_kor : '슬로셔',
+                content : '당신에게는 정면 싸움보다 좋은 위치를 선점하고 \n 일방적으로 목표를 타격하는 슬로셔가 어울립니다!',
+                img : require('../assets/weaponImg/slosher.jpg'),
             },
         ]
       }
     },
 
+    mounted(){
+      this.setWeaponData(this.weaponType);
+    },
+
     methods : {
-        setWeaponData : function(){
-            
+        setWeaponData : function(e){
+            for(let i=0; i<this.weaponArray.length; i++){
+                if(e === this.weaponArray[i].name){
+                    this.weaponName = this.weaponArray[i].name_kor;
+                    this.weaponContent = this.weaponArray[i].content.replace(/(?:\r\n|\r|\n)/g, '<br />');
+                    this.weaponImg = this.weaponArray[i].img;
+                }
+            }
         }
     },
 

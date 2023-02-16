f0f121f5f8893e6e7381d2bd98cd34f6d3cff7d7
uonod1323 <uonod1323@naver.com>
2023-02-16 10:55:33 +0900

    디테일페이지(구조상 실패)

---
+++ src/components/WeaponDetailPage.vue
@@ -0,0 +1,28 @@
+<template>
+     <div class = "container">
+    <div class="content-container">
+      <div class="question-container">
+        {{ this.weaponType }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'WeapontypePage',
+    data(){
+      return {
+        weaponTypeName    : '',
+        
+      }
+    },
+    props : {
+        weaponType : String,
+    },
+}
+</script>
+
+<style>
+
+</style>
